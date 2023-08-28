3ab274772752c523dec8048efda311f7ba99a0c1
Mateus Gustavo <githubmateus@gmail.com>
2023-08-28 00:41:33 -0300

    refactor: responsiveness NextDays

--- src/components/NextDays.tsx
+++ src/components/NextDays.tsx
@@ -2,11 +2,11 @@ import Day from './Day'
 
 export default function NextDays() {
   return (
-    <div className="flex w-full max-w-[630px] flex-col gap-5 rounded-xl bg-base-gray-800 p-6 mobile:p-3">
-      <span className="font-nunito text-base text-base-gray-400 mobile:hidden">
+    <div className="flex h-max w-full max-w-[630px] flex-col gap-5 rounded-xl bg-base-gray-800 p-6 mobile2:p-4">
+      <span className="tablet2:hidden font-nunito text-base text-base-gray-400">
         Previsão para 5 dias
       </span>
-      <div className="flex w-full justify-center">
+      <div className="flex h-full w-full justify-center">
         <Day />
         <Day />
         <Day />
